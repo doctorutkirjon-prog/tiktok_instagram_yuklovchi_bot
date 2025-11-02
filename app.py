a1c9781a39e6ed3b270181dba8e58fa3ed825ece
doctorutkirjon-prog <doctorutkirjon@gmail.com>
2025-11-03 01:13:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,10 @@ def getMessage():
     return '', 200
 
 # --- Ишга тушириш ---
+import os
+
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))
+    server.run(host="0.0.0.0", port=port)
+
 
